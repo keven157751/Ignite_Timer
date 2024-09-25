78f662fbc9b34f74c22f2eb8ee95c7b2f20eb51e
notekeven <jf5143437@gmail.com>
2024-09-25 01:01:36 -0300

    adjusts Package

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { App } from './App.tsx'
 import ReactDOM from 'react-dom/client'
 
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// const router = createBrowserRouter([]); 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
 )
